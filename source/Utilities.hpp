d6991b2ff60f03551e38668e991523ec796c1690
Jesus Guio <jfranciscoguio@gmail.com>
2020-12-07 19:34:38 -0500

    Add shaders and change CMake file for compile shaders to spr-v

--- source/Utilities.hpp
+++ source/Utilities.hpp
@@ -1,6 +1,10 @@
 #ifndef VULKAN_COURSE_UTILITIES_HPP
 #define VULKAN_COURSE_UTILITIES_HPP
 
+
+#include <fstream>
+
+
 const std::vector<const char *> deviceExtensions = {
         VK_KHR_SWAPCHAIN_EXTENSION_NAME
 };
@@ -25,5 +29,34 @@ struct SwapChainImage {
     VkImage image;
     VkImageView imageView;
 };
+
+static std::vector<char> readFile(const std::string& fileName) {
+    // Open stream from given file
+    // std::ios::binary tells stream to read file as binary
+    // std::ios::ate tells stream to start reading from end of file
+    std::ifstream file(fileName, std::ios::binary | std::ios::ate);
+
+    // Check if file stream successfully opened
+    if (!file.is_open()) {
+        throw std::runtime_error("Failed to open " + fileName + " file");
+    }
+
+    // Get current read position and use to resize file buffer
+    size_t fileSize = static_cast<size_t>(file.tellg());
+    std::vector<char> fileBuffer(fileSize);
+
+    // Move read position (seek to) the start of the file
+    file.seekg(0);
+
+
+    // Read the file data into the buffer (stream "fileSize" in total)
+    file.read(fileBuffer.data(), fileSize);
+
+    // Close the stream
+    file.close();
+
+    return fileBuffer;
+}
+
 
 #endif
